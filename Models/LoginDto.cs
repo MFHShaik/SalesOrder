93097c8f8777ab5da756a8fac12bab76d682823e
Mohammed <fuzailshaik22@gmail.com>
2024-10-17 21:22:12 +0200

    Adding login view

---
+++ Models/LoginDto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace SalesOrders.Models
+{
+    public class LoginDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+
+}
